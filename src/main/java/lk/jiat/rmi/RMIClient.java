1d625f9951e733ae263926e37b886741df53de91
chanuka-rukshan <chanukarukshan81@gmail.com>
2026-06-07 13:29:28 +0530

    Done

--- src/main/java/lk/jiat/rmi/RMIClient.java
+++ src/main/java/lk/jiat/rmi/RMIClient.java
@@ -1,7 +1,23 @@
 package lk.jiat.rmi;
 
+import lk.jiat.rmi.client.Message;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class RMIClient {
     public static void main(String[] args) {
-
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 6666);
+            String[] list = registry.list();
+            for (String s : list) {
+                System.out.println(s);
+            }
+            Message message = (Message) registry.lookup("message_service");
+            String m = message.hello();
+            System.out.println(m);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
